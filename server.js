eb9ff2673e8a19775b4a7ba4e22a1b96e70aa752
Shubham <pespro2727@gmail.com>
2025-01-16 21:11:51 +0530

    atlas db connection established

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+// Now that .env file is just a text file so far but to connect it with node
+// we use dotenv package and import it.
+// Now it will read from config.env file and save them to our node env variables.
+// to make these env var through out the app we need to read them before we import app.
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
+// To see the env variable
+// console.log(process.env);
+
+// Lets set the pswd in our connection url
+const db = process.env.DB_CONNECTION_URL.replace(
+  '<db_password>',
+  process.env.DB_PASSWORD
+);
+
+// connect to db
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('db connection successfull');
+  });
+
 // port No
-const port = 3000;
+const port = process.env.PORT;
 
 // start the server:
 // Listen to incoming req on this port no
